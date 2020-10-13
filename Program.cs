e786426d553145b88a2670f1042729e1690420c2
Samuel Karlström <sam.karlstrm@gmail.com>
2020-10-13 16:14:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ovning1_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
